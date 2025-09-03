56ab601590769df6f2dfe0a6e0077dd224b71353
SocialStrategy <yarrickau@gmail.com>
2025-09-03 23:18:34 +0700

    Remove Featured Content section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import HeroSection from '@/components/HeroSection'
-import FeaturedContent from '@/components/FeaturedContent'
 import FeaturedCommunityPost from '@/components/FeaturedCommunityPost'
 import SocialLinks from '@/components/SocialLinks'
 import AffiliateLinks from '@/components/AffiliateLinks'
@@ -9,7 +8,6 @@ export default function Home() {
     <main className="min-h-screen">
       <HeroSection />
       <FeaturedCommunityPost />
-      <FeaturedContent />
       <SocialLinks />
       <AffiliateLinks />
     </main>
